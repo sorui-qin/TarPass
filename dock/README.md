05152ed21308892f5cd90624283c82fefca584bf
sorui-qin <842220994@qq.com>
2025-03-05 21:09:32 +0800

    update_250305

---
+++ dock/README.md
@@ -0,0 +1,13 @@
+## Introduction to `Dock`
+
+### `autobook_scripts`
+The files in this directory are primarily used for the receptor preparation of the HDAC6 target and are generally not used.  
+
+### `maps`
+This directory contains the affinity maps for all test targets.  
+We recommend directly downloading and using the affinity maps from this directory when performing docking with AutoDock-Vina to save time.  
+All map files can be downloaded via [Here](todo)
+
+### `pdbqtfiles`
+This directory includes the pdbqt files for all targets.  
+The pdbqt files can be used for automatical preparation, except for the affinity maps of HDAC6 and the two holo proteins.
--- dock/dock_test.py
+++ dock/dock_test.py
@@ -1,7 +1,7 @@
 '''
 Author: Rui Qin
 Date: 2025-03-01 14:58:38
-LastEditTime: 2025-03-05 09:20:47
+LastEditTime: 2025-03-05 21:01:02
 Description: 
 '''
 from vina import Vina
@@ -10,9 +10,9 @@ from meeko import PDBQTWriterLegacy
 from meeko import PDBQTMolecule
 from meeko import RDKitMolCreate
 from rdkit import Chem
-import subprocess
 import glob
 import tqdm
+import json
 from pathlib import Path
 import numpy as np
 
@@ -35,38 +35,60 @@ def lig_prep(lig_sdf):
     return pdbqt_string
 
 
-def rec_prep(rec_prefix):
-    command_prep = f'''mk_prepare_receptor.py \
-                    -i {rec_prefix}.pdb \
-                    -o {rec_prefix} -p'''
-    proc = subprocess.Popen(command_prep, shell=True, stdin=subprocess.PIPE, 
-                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout_data, stderr_data = proc.communicate()
-    print(stdout_data.decode(), stderr_data.decode())
+def maps_prep(pocket, box_size=[20, 20, 20]):
+    assert pocket != 'HDAC6', 'Preparation for HDAC6 should follow `Target/HDAC6/preprocess.ipynb`'
 
+    rec_pdbqt = glob.glob(f'dock/pdbqtfiles/{pocket}_*.pdbqt')[0]
+    try: # for apo structure
+        ref_sdf = glob.glob(f'Targets/{pocket}/*ligand*.sdf')[0]
+        center = sdf2centroid(ref_sdf)
+    except: # for holo and AlphaFold structure
+        center_json = glob.glob(f'Targets/{pocket}/center.json')[0]
+        with open(center_json, "r") as f:
+            center = json.load(f)["center"]
 
-def dock(pdbqt_string, rec_prefix, center):
-    box_size = [20, 20, 20]
+    Path(f'dock/maps/{pocket}').mkdir(exist_ok=True)
+
     v = Vina(cpu=0, seed=42)
-    v.set_receptor(f'{rec_prefix}.pdbqt')
-    v.set_ligand_from_string(pdbqt_string)
+    v.set_receptor(rec_pdbqt)
     v.compute_vina_maps(center, box_size)
-    v.dock(exhaustiveness=32, n_poses=10)
-    vina_output_string = v.poses(n_poses=10)
-    pdbqt_mol = PDBQTMolecule(vina_output_string, skip_typing=True)
-    rdkitmol_list = RDKitMolCreate.from_pdbqt_mol(pdbqt_mol)
-    return rdkitmol_list
+    v.write_maps(map_prefix_filename=f'dock/maps/{pocket}/{pocket}', overwrite=True)
 
 
-def dock_zinc(pdbqt_string, maps):
-    v = Vina(sf_name='ad4',seed=42)
+def dock(pdbqt_string, maps, mode='dock', sf_name='vina', seed=0, exhaust=32, n_poses=1, verbose=0) -> tuple[float, list]:
+    """_summary_
+
+    Args:
+        pdbqt_string (_type_): _description_
+        maps (_type_): _description_
+        mode (str, optional): _description_. Defaults to 'dock'.
+        sf_name (str, optional): _description_. Defaults to 'vina'.
+        seed (int, optional): _description_. Defaults to 0.
+        exhaust (int, optional): _description_. Defaults to 32.
+        n_poses (int, optional): _description_. Defaults to 1.
+
+    Returns:
+        tuple[float, list]: _description_
+    """
+    v = Vina(sf_name=sf_name, cpu=0, seed=seed, verbosity=verbose)
     v.set_ligand_from_string(pdbqt_string)
     v.load_maps(maps)
-    v.dock(exhaustiveness=32, n_poses=10)
-    vina_output_string = v.poses(n_poses=10)
-    pdbqt_mol = PDBQTMolecule(vina_output_string, skip_typing=True)
-    rdkitmol_list = RDKitMolCreate.from_pdbqt_mol(pdbqt_mol)
-    return rdkitmol_list
+    if mode == 'score_only':
+        score = v.score()[0]
+        return score, None
+    if mode == 'dock':
+        v.dock(exhaustiveness=exhaust, n_poses=n_poses)
+        score = v.energies(n_poses=n_poses)[0][0]
+        vina_output_string = v.poses(n_poses=n_poses)
+        pdbqt_mol = PDBQTMolecule(vina_output_string, skip_typing=True)
+        poses = RDKitMolCreate.from_pdbqt_mol(pdbqt_mol)
+        return score, poses
+    
+
+def write_poses(poses, filename):
+    with Chem.SDWriter(filename) as w:
+        for mol in poses:
+            w.write(mol)
 
 
 def cal_RMSD(ref_mol, check_mol):
@@ -77,53 +99,32 @@ def cal_RMSD(ref_mol, check_mol):
 
 if __name__ == '__main__':
     target_dirs = sorted(glob.glob('./Targets/*/'))
-    target_dirs.remove('./Targets/5HT2A_AF/')
-    target_dirs.remove('./Targets/BRD4_holo/')
-    target_dirs.remove('./Targets/HDAC6/')
+    target_dirs.remove('./Targets/5HT2A-AF/')
+    target_dirs.remove('./Targets/BRD4-holo/')
 
-    rmsd_list = []
     for target in tqdm.tqdm(target_dirs):
-        ref_sdf = glob.glob(f'{target}/*ligand*.sdf')[0]
-        rec_pdbs = glob.glob(f'{target}/*rec*.pdb')
-        if len(rec_pdbs) == 1:
-            rec_pdb = rec_pdbs[0]
-        else:
-            rec_pdb = [rec for rec in rec_pdbs 
-                       if not rec.endswith('empty.pdb')][0]
-        rec_prefix = rec_pdb.replace('.pdb', '')
-        redock_sdf = f'{target}/redock_32-multi.sdf'
+        pocket = Path(target).name
 
-        # Ligprep
+        #ligprep
+        ref_sdf = glob.glob(f'{target}/*ligand*.sdf')[0]
         pdbqt_string = lig_prep(ref_sdf)
 
-        # Recprep
-        if not Path(f'{rec_prefix}.pdbqt').exists():
-            rec_prep(rec_prefix)
+        #recprep
+        maps_dir = f'dock/maps/{pocket}/'
+        if not Path(maps_dir).exists():
+            maps_prep(pocket)
         
-        # Docking
-        assert Path(f'{rec_prefix}.pdbqt').exists()
-        if not Path(redock_sdf).exists():
-            center = sdf2centroid(ref_sdf)
-            docked_mols  = dock(pdbqt_string, rec_prefix, center)
-        
-            # Calculate RMSD
-            #ref_mol = Chem.SDMolSupplier(ref_sdf)[0]
-            #rmsd_list.append(cal_RMSD(ref_mol, docked_mol))
-            with Chem.SDWriter(redock_sdf) as w:
-                for mol in docked_mols:
+        #dock
+        assert Path(maps_dir).exists(), 'Affinity map files do not exist!'
+        outfile = f'{target}/redock-32.sdf'
+        if not Path(outfile).exists():
+            sf_name = 'ad4' if pocket == 'HDAC6' else 'vina'
+            maps = f'dock/maps/{pocket}/{pocket}'
+            _, poses = dock(pdbqt_string=pdbqt_string,
+                            maps=maps,
+                            sf_name=sf_name,
+                            seed=42,
+                            verbose=1)
+            with Chem.SDWriter(outfile) as w:
+                for mol in poses:
                     w.write(mol)
-    
-    # Zinc dock
-    target = './Targets/HDAC6/'
-    ref_sdf = glob.glob(f'{target}/*ligand*.sdf')[0]
-    
-    pdbqt_string = lig_prep(ref_sdf)
-    rec_pdb = glob.glob(f'{target}/*rec*.pdb')[0]
-
-    rec_prefix = rec_pdb.replace('.pdb', '')
-    dock_mols = dock_zinc(pdbqt_string, maps=f'{target}/maps/HDAC6_8bjk_rec_A_tz')
-
-    redock_sdf = f'{target}/redock_32-multi.sdf'
-    with Chem.SDWriter(redock_sdf) as w:
-        for mol in dock_mols:
-            w.write(mol)
